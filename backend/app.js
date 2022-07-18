591d23bbeabef98b7fa6a0758d6d11fac0ccab85
Vladislav Yevtushenko <vladislavyevtushenko@yandex.ru>
2022-07-18 22:41:53 +0300

    fix cors module

--- backend/app.js
+++ backend/app.js
@@ -22,7 +22,7 @@ mongoose.connect('mongodb://localhost:27017/mestodb', {
   useUnifiedTopology: true,
 });
 
-app.use(cors);
+app.use(cors({ origin: 'https://mesto.by.vlad.nomorepartiesxyz.ru', credentials: true }));
 
 // app.use(cors({
 //   origin: allowedCors,
